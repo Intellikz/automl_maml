54c5d426b26b4b4a7ef1ea01818a3706ec7d051e
Intellikz <Intellikzskype@gmail.com>
2020-11-11 16:11:18 +0100

    init function

--- assignment.py
+++ assignment.py
@@ -20,6 +20,15 @@ class MAML:
         Number of updates to make to the initialization parameters in the inner-loop
         """
         
+        # Number of updates to make in the inner-loop (on a given task)
+        self.num_steps = num_steps
+        # Loss function to optimize
+        self.loss_fn = nn.MSELoss()
+        # Shape of the base-learner network 
+        self.initialization = BaseLearner.random_initial_weights()
+        # Initialize the meta-optimizer, which will update the initialization point
+        self.meta_optimizer = optim.Adam(self.initialization)
+        
         # Keep a history of losses on the query set
         self.history = [] # DO NOT DELETE THIS. 
         
